504a933340eee7c65b6305377eb0fdadcf942a1f
Boris <gh@gerretzen.eu>
2025-04-05 23:04:25 +0200

    final touches

--- build/Build.cs
+++ build/Build.cs
@@ -220,7 +220,7 @@ class Build : NukeBuild
             DotNetNuGetPush(s => s
                 .SetSource(NugetSource)
                 .SetApiKey(NugetApiKey)
-                .CombineWith(ArtifactsDirectory.GlobFiles("*.nupkg"), (s, v) => s
+                .CombineWith(ArtifactsDirectory.GlobFiles("*.nupkg", "*.snupkg"), (s, v) => s
                     .SetTargetPath(v)
                 )
             );
