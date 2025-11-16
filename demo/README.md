7ab2e1878216f608899b9ecc128372fe2d5d7706
Boris Gerretzen <gh@gerretzen.eu>
2025-11-16 13:42:46 +0100

    Chore/refactor demo (#13)
    * Refactor solution, add blazor server demo
    
    * Clean up
    
    * Improve readme demo

---
+++ demo/README.md
@@ -0,0 +1,5 @@
+﻿# Demos
+
+In this folder you can find the demo projects for the library.
+In `SimpleBlazorMultiselect.Demo.Shared` you can find the implementation of the various pages of the demo app.
+The other two projects showcase how to use the library in Blazor WebAssembly and Blazor Server applications.
---
+++ demo/SimpleBlazorMultiselect.Demo.Server/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
